90b0d0fbfb68f202e7dc196e9494d5c30d841005
Joshua-Keely <95314775+Joshua-Keely@users.noreply.github.com>
2024-10-16 09:32:40 +0000

    Initial

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, session, redirect, url_for
+
+app = Flask(__name__)
+app.secret_key = 'secret'
+
+@app.route('/')
+def index():
+    if 'counter' not in session:
+        session['counter'] = 0
+    return render_template('index.html')
+
+@app.route('/add', methods=['POST'])
+def add():
+    session['counter'] += 1
+    return redirect(url_for('index'))
+
+@app.route('/subtract', methods=['POST'])
+def subtract():
+    session['counter'] -= 1
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
