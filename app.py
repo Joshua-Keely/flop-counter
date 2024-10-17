3300e4a1387862c8d5a16e45eeb6deee8cd6c55e
Joshua <keelyj@etu.univ-grenoble-alpes.fr>
2024-10-17 13:19:53 +0200

    Update(Update CI)

--- app.py
+++ app.py
@@ -3,11 +3,13 @@ from flask import Flask, render_template, session, redirect, url_for
 app = Flask(__name__)
 app.secret_key = 'secret'
 
+
 @app.route('/')
 def index():
     with open('value.txt', 'r') as f:
         counter = f.read()
     return render_template('index.html', counter=counter)
+
 
 @app.route('/add', methods=['POST'])
 def add():
@@ -18,6 +20,7 @@ def add():
         f.write(str(counter))
     return str(counter)
 
+
 @app.route('/subtract', methods=['POST'])
 def subtract():
     with open('value.txt', 'r') as f:
@@ -26,11 +29,14 @@ def subtract():
     with open('value.txt', 'w') as f:
         f.write(str(counter))
     return str(counter)
+
+
 @app.route('/get_counter', methods=['GET'])
 def get_counter():
     with open('value.txt', 'r') as f:
         counter = f.read()
     return counter
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
